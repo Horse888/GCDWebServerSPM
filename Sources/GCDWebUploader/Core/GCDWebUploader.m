ff676e14a9b972f926b4254eccf12c64a5624c16
ZY <749939+Horse888@users.noreply.github.com>
2024-06-24 13:55:44 +0800

    Update GCDWebUploader.m

--- Sources/GCDWebUploader/Core/GCDWebUploader.m
+++ Sources/GCDWebUploader/Core/GCDWebUploader.m
@@ -66,7 +66,7 @@ NS_ASSUME_NONNULL_END
 
 - (instancetype)initWithUploadDirectory:(NSString*)path {
   if ((self = [super init])) {
-    NSString* bundlePath = [[NSBundle mainBundle] pathsForResourcesOfType:@"bundle" inDirectory:@"GCDWebServerSPM_GCDWebUploader.bundle"];
+    NSString* bundlePath = [[[NSBundle mainBundle] pathsForResourcesOfType:@"bundle" inDirectory:@"GCDWebServerSPM_GCDWebUploader.bundle"] first];
     if (bundlePath == nil) {
       return nil;
     }
